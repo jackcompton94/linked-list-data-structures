c31fc72e907b408d135d14709338eee5c5e28ae6
jackcompton94 <jackcompton94@gmail.com>
2022-12-09 17:38:12 -0600

    Initial Commit

---
+++ linked-list-data-structures/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  linked-list-data-structures
+//
+//  Created by Jack Compton on 12/9/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
